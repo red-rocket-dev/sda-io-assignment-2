e747ddf391eeeaceca17d884084e4a8b97d72e3f
Jakub <kubplon@gmail.com>
2020-03-07 00:25:57 +0100

    Assignment IO 2

---
+++ src/main/java/pl/sda/io/Main.java
@@ -0,0 +1,11 @@
+package pl.sda.io;
+public class Main {
+    public static void main(String[] args) {
+        /*
+        1. Znajdz w folderze my-files pliki, które zawierają słowo "coś" i wypisz nazwy tych plikow na konsole
+        1*. Używając wyrażenia regularnego poza nazwa pliku wypisz też słowo występujące po "coś" (wystarczy po pierwszym wystąpieniu)
+        2. Poza wypisaniem nazw plików wypisz też ilość wystąpień tego słowa w danym pliku w formacie <nazwa>:<ilość wystąpień>
+        3. Zamiast wypisywać na konsolę zapis rezultat do pliku
+         */
+    }
+}
